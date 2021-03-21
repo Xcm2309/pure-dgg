2912b02170731a4e48e1ebd961d4c6314df37098
Xcm2309 <Xcm2309@gmail.com>
2021-03-21 15:34:05 -0400

    Remove empty messages and greentext

--- content.js
+++ content.js
@@ -1,26 +1,42 @@
 var chat_win_main = document.getElementById("chat-win-main");
 var chat_lines = chat_win_main.childNodes[0];
 
-chat_lines.addEventListener("DOMNodeInserted", function (e) {
+function remove_emotes_from_message(chat_message)
+{
+    chat_message_text = chat_message.getElementsByClassName("text");
+    for (var text_index = 0; text_index < chat_message_text.length; text_index++)
+    {
+        chat_message_emotes = chat_message_text[text_index].getElementsByClassName("emote");
+        var num_emotes = chat_message_emotes.length;
+        var hasEmote = num_emotes != 0;
+        for (var emote_index = num_emotes - 1; emote_index >= 0; emote_index--) {
+            chat_message_emotes[emote_index].remove();
+        }
 
-    var chat_message = e.target;
-    
-    // Remove combo messages
-    if (chat_message.classList.contains("msg-emote")) {
-        chat_lines.removeChild(chat_message);
-    } else {
-        chat_message_text = chat_message.getElementsByClassName("text");
-        for (var i = 0; i < chat_message_text.length; i++) {
-            chat_message_emotes = chat_message_text[i].getElementsByClassName("emote");
-            for (var emote_index = 0; emote_index < chat_message_emotes.length; emote_index++) {
-                chat_message_text[i].removeChild(chat_message_emotes[emote_index]);
-            }
-
-            if (chat_message_text[i].childNodes.length == 0) {
-                chat_lines.removeChild(chat_message);
+        if (hasEmote) {
+            console.log(chat_message_text[text_index].textContent);
+            if (chat_message_text[text_index].textContent == " 7") {
+                console.log("salute detected");
+                chat_message.remove();
             }
         }
     }
+}
 
+function remove_empty_messages(chat_message) {
+
+    chat_message_text = chat_message.getElementsByClassName("text");
+    if (chat_message_text[0].textContent == "") {
+        chat_message.remove();
+    }
+}
+
+chat_lines.addEventListener("DOMNodeInserted", function (e) {
+
+    var chat_message = e.target;
+    if (chat_message.classList.contains("msg-chat")) {
+        remove_emotes_from_message(chat_message);
+        remove_empty_messages(chat_message);
+    }
 }, false);
 
